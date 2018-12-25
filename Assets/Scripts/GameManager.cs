6a011740445e93ad3b0e24352809fb16935b75f4
ifk <ifknord@gmail.com>
2018-12-25 07:45:42 +0300

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public int lives;
+    public int score;
+    public Text livesText;
+    public Text scoreText;
+    public bool gameOver;
+    public GameObject gameOverPanel;
+
+    // Use this for initialization
+    void Start () {
+        livesText.text = "Lives: " + lives;
+        scoreText.text = "score: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateLives(int changeInLives)
+    {
+        lives += changeInLives;
+
+        if (lives <= 0)
+        {
+            lives = 0;
+            GameOver();
+        }
+
+        livesText.text = "Lives: " + lives;
+    }
+
+    public void UpdateScore(int points)
+    {
+        score += points;
+
+        scoreText.text = "score: " + score;
+    }
+
+    void GameOver()
+    {
+        gameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+}
